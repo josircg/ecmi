9df658e00f6b5aff53db6a3cb0ec961ac3d33185
Josir Cardoso Gomes <josircg@gmail.com>
2023-05-23 07:52:10 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -4,7 +4,11 @@ import pandas as pd
 st.title('Teste ECMI 2')
 
 st.write("Tabela")
-st.write(pd.DataFrame({
+
+dataframe = pd.DataFrame({
     'Nome': ['Josir', 'Bruno', 'Bruna', 'Anna'],
     'Salário': [10, 20, 30, 40]
-}))
+})
+dataframe.style.highlight_max(axis=0)
+
+st.write(dataframe)
