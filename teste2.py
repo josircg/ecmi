12df2517b4c005a02335b74775952f70760f37f5
Josir Cardoso Gomes <josircg@gmail.com>
2023-05-23 08:00:26 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -12,3 +12,9 @@ dataframe = pd.DataFrame({
 dataframe.style.highlight_max(axis=0)
 
 st.write(dataframe)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
