30e15c7e8fd8395ad77fc983fa32ce28ce64fb0f
Josir Cardoso Gomes <josircg@gmail.com>
2024-05-20 20:01:56 -0300

    Update and rename wordcloud.py to nuvem.py

--- nuvem.py
+++ nuvem.py
@@ -3,12 +3,12 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 texto = st.text_input("Digite a letra de uma música sem pontuações: ")
-wc = WordCloud(width=800, height=400).generate(texto.lower())
-
-plt.imshow(wc, interpolation='bilinear')
-plt.axis("off")
-plt.show()
-st.pyplot(plt)
+if len(texto) > 0:
+    wc = WordCloud(width=800, height=400).generate(texto.lower())
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    st.pyplot(plt)
 
 # wc.to_file("wc.png")
 # st.image("wc.png", use_column_width=True)
